e1eeba4fb733dddfd884fc759c6bb342b8d9885a
Boegie19 <Bramboegheim@gmail.com>
2023-10-13 12:30:59 +0200

    Fix field

--- strapi-plugin-multitenancy/admin/src/index.js
+++ strapi-plugin-multitenancy/admin/src/index.js
@@ -1,8 +1,8 @@
-import { prefixPluginTranslations } from '@strapi/helper-plugin';
-import pluginPkg from '../../package.json';
-import pluginId from './pluginId';
-import Initializer from './components/Initializer';
-import PluginIcon from './components/PluginIcon';
+import { prefixPluginTranslations } from "@strapi/helper-plugin";
+import pluginPkg from "../../package.json";
+import pluginId from "./pluginId";
+import Initializer from "./components/Initializer";
+import PluginIcon from "./components/PluginIcon";
 
 const name = pluginPkg.strapi.name;
 
@@ -16,7 +16,9 @@ export default {
         defaultMessage: name,
       },
       Component: async () => {
-        const component = await import(/* webpackChunkName: "[request]" */ './pages/App');
+        const component = await import(
+          /* webpackChunkName: "[request]" */ "./pages/App"
+        );
 
         return component;
       },
--- strapi-plugin-multitenancy/server/register.js
+++ strapi-plugin-multitenancy/server/register.js
@@ -1,5 +1,21 @@
-'use strict';
+"use strict";
 
 module.exports = ({ strapi }) => {
-  // register phase
+  strapi.customFields.register({
+    name: "tenant_id",
+    plugin: "multitenancy",
+    inputSize: { default: 4, isResizable: true },
+    type: "string",
+  });
+  for (const indexedCT of Object.values(strapi.contentTypes)) {
+    strapi.contentTypes[indexedCT.uid].attributes["tenant_id"] = {
+      type: "string",
+      configurable: false,
+      writable: false,
+      visible: false,
+    };
+    strapi.contentTypes[indexedCT.uid].__schema__.attributes["tenant_id"] = {
+      type: "string",
+    };
+  }
 };
