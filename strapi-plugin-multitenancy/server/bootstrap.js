1ea3939a7ea24fd2f272ca76e81b71db09355a4c
Boegie19 <Bramboegheim@gmail.com>
2023-10-14 17:11:02 +0200

    fix findOne

--- strapi-plugin-multitenancy/server/bootstrap.js
+++ strapi-plugin-multitenancy/server/bootstrap.js
@@ -13,15 +13,20 @@ module.exports = async ({ strapi }) => {
   await strapi.admin.services.permission.actionProvider.registerMany(actions);
 
   strapi.entityService.decorate((defaultService) => ({
-    delete: async (uid, id, opts) => {},
-
-    deleteMany: async (uid, opts) => {},
-
-    findOne: async (uid, id, opts) => {},
+    findOne: async (uid, id, opts) => {
+      const fullCTX = strapi.requestContext.get();
+      const result = await strapi.db.query(uid).findMany({
+        where: { id: id, tenant_id: fullCTX.state.user.tenant_id },
+        select: [],
+      });
+      if (result.length === 0) {
+        return;
+      }
+      return defaultService.findOne(uid, id, opts);
+    },
     wrapParams: async (opts, ctx) => {
       const wrappedParams = await defaultService.wrapParams(opts, ctx);
       const fullCTX = strapi.requestContext.get();
-      //console.log(fullCTX);
       let customFilter = {};
       if ((fullCTX.state.auth.name = "admin")) {
         //transformParamsToQuery
@@ -29,7 +34,6 @@ module.exports = async ({ strapi }) => {
           tenant_id: fullCTX.state.user.tenant_id,
         };
       }
-      console.log(fullCTX.state.user);
       if (wrappedParams.data) {
         delete wrappedParams.data.tenant_id;
       }
