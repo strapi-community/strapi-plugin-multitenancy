5d8b58d832e37217994679d1ad96074fa8a189a1
Boegie19 <Bramboegheim@gmail.com>
2023-10-15 19:13:10 +0200

    rename policycontext to ctx,

--- strapi-plugin-multitenancy/server/policies/is-same-tenant.js
+++ strapi-plugin-multitenancy/server/policies/is-same-tenant.js
@@ -1,15 +1,13 @@
-module.exports = async (policyContext, config, { strapi }) => {
-  if (policyContext.state.user) {
-    const result = await strapi.query("admin::api-token").findMany({
-      select: [],
-      where: {
-        tenant_id: ctx.state.auth.credentials.tenant_id,
-        id: ctx.request.params.id,
-      },
-    });
-    if (result.length !== 0) {
-      return true;
-    }
+module.exports = async (ctx, config, { strapi }) => {
+  const result = await strapi.query("admin::api-token").findMany({
+    select: [],
+    where: {
+      tenant_id: ctx.state.auth.credentials.tenant_id,
+      id: ctx.request.params.id,
+    },
+  });
+  if (result.length !== 0) {
+    return true;
   }
 
   return false;
