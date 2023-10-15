3f847e2c368b9324d2a5d7d3ebffa7b618429154
Boegie19 <Bramboegheim@gmail.com>
2023-10-15 18:25:42 +0200

    remove middleware from index

--- strapi-plugin-multitenancy/server/middlewares/index.js
+++ strapi-plugin-multitenancy/server/middlewares/index.js
@@ -1,8 +1,6 @@
 "use strict";
 
 const injectTenantId = require("./inject_tenant_id");
-const redoListApiToken = require("./redo_list_api_token");
 module.exports = {
   injectTenantId,
-  redoListApiToken,
 };
